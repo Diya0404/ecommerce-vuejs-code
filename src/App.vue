40e2d5f3484326bacd6ad4fed7005e9c6962cea5
Priyanshi Gupta <priyanshialambagh@gmail.com>
2024-04-22 10:05:20 +0530

    added files

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+  <NavbarComponent />
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view
+  :baseURL="baseURL"
+  :categories="categories"
+  :products="products"
+  >
+  </router-view>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import NavbarComponent from "./components/NavbarComponent.vue";
+import axios from 'axios';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components: { NavbarComponent },
+  data() {
+    return {
+      baseURL : "https://limitless-lake-55070.herokuapp.com/",
+      products: [],
+      categories: []
+    }
+  },
+  methods: {
+    async fetchData() {
+
+      // api call to get all the categories
+      await axios.get(this.baseURL + "category/")
+      .then(res => {
+        this.categories = res.data
+      }).catch((err) => console.log('err', err));
+
+      // api call to get the products
+
+      await axios.get(this.baseURL + "product/")
+      .then(res => {
+        this.products = res.data
+      }).catch((err) => console.log('err', err));
+    }
+  },
+  mounted() {
+    this.fetchData();
   }
-}
+};
 </script>
 
 <style>
@@ -21,6 +56,19 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
+NavbarComponent
